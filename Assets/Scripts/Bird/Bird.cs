20ebe96947b4253a412e8402f86c3f296b0cde99
userUnity <unitvladimir@gmail.com>
2022-05-18 23:11:28 +1000

    go

---
+++ Assets/Scripts/Bird/Bird.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(BirdMover))]
+public class Bird : MonoBehaviour
+{
+    private BirdMover _birdMover;
+    private int _score;
+
+    private void Start()
+    {
+        _birdMover = GetComponent<BirdMover>();
+    }
+
+    public void ResetLevel()
+    {
+        _score = 0;
+        _birdMover.ResetBird();
+    }
+
+    public void Die()
+    {
+        Debug.Log("I'm die");
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scripts/BirdTracker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+
+public class BirdTracker : MonoBehaviour
+{
+    [SerializeField] private Bird _bird;
+    [SerializeField] private float _offSetX;
+
+    private void Update()
+    {
+        transform.position =
+            new Vector3(_bird.transform.position.x - _offSetX, transform.position.y, transform.position.z);
+    }
+}
